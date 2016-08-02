861a008f16152a7ceabb1fcd27541092e0aa605b
vic10us <keith@dexs.ca>
2016-08-02 10:39:01 -0400

    Adding base Unit Test project

---
+++ Src/Metrics.NET.PrometheusTests/PrometheusReportExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Metrics.NET.Prometheus.Tests
+{
+    [TestClass()]
+    public class PrometheusReportExtensionsTests
+    {
+        [TestMethod()]
+        public void WithPrometheusEndpointReportTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ Src/Metrics.NET.PrometheusTests/PrometheusReportTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Metrics.NET.Prometheus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics.NET.Prometheus.Tests
+{
+    [TestClass()]
+    public class PrometheusReportTests
+    {
+        [TestMethod()]
+        public void RenderMetricsTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
