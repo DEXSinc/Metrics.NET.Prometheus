68aad3cecf683260861ec0c84b2f96d4b6bf0d7f
vic10us <keith@dexs.ca>
2016-11-28 10:49:41 -0500

    Exposing encoding options in PrometheusReportExtensions

--- Src/Metrics.NET.Prometheus/PrometheusReportExtensions.cs
+++ Src/Metrics.NET.Prometheus/PrometheusReportExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Metrics.Reports;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using Metrics.Reports;
 using Metrics.Visualization;
 
 namespace Metrics.NET.Prometheus
@@ -14,10 +16,22 @@ namespace Metrics.NET.Prometheus
         /// <param name="config"></param>
         /// <param name="path"></param>
         /// <returns></returns>
-        public static MetricsEndpointReports WithPrometheusEndpointReport(this MetricsEndpointReports config,
-            string path = "/prometheus")
+        public static MetricsEndpointReports WithPrometheusEndpointReport(this MetricsEndpointReports config, string path = "/prometheus")
         {
-            return config.WithEndpointReport(path, (d, h, c) => new MetricsEndpointResponse(PrometheusReport.RenderMetrics(d, h), "text/plain"));
+            return WithPrometheusEndpointReport(config, path, Encoding.ASCII);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="path"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static MetricsEndpointReports WithPrometheusEndpointReport(this MetricsEndpointReports config,
+            string path, Encoding encoding)
+        {
+            return config.WithEndpointReport(path, (d, h, c) => new MetricsEndpointResponse(PrometheusReport.RenderMetrics(d, h), "text/plain", encoding));
         }
     }
 }
