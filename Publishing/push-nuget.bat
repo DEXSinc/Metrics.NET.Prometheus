e6d931c4758fbcc4e7896a1907c6182abbbbf975
vic10us <siridev@dexs.ca>
2018-05-05 09:54:26 -0400

    Updating all refs to v0.5.5

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,1 +1,1 @@
-..\.nuget\NuGet.exe push Metrics.NET.Prometheus.0.5.1.nupkg -Source https://www.nuget.org/api/v2/package
+..\.nuget\NuGet.exe push Metrics.NET.Prometheus.0.5.5.nupkg -Source https://api.nuget.org/v3/index.json
